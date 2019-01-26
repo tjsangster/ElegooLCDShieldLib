6355070baa38a37999518045a55cca6a0e12b70e
Tyler <tjsangster@gmail.com>
2019-01-26 12:15:14 -0600

    Simplify circle building.

--- test.cpp
+++ test.cpp
@@ -1,6 +1,4 @@
 
-
-
 #include <avr/io.h>
 #include <util/delay.h>
 #include <math.h>
@@ -9,14 +7,19 @@
 
 LCDShield* lcd;
 
+
 int main(void) {
 
     srand(42);
 
     lcd = &LCDShield::getInstance();
 
-    unsigned char i;
+    unsigned int i;
     const unsigned char max_circle_radius = LCDShield::SCREEN_WIDTH / 2;
+    unsigned int stable[256];
+    
+    for(i=0; i < 256; ++i)
+        stable[i]  = square(i);
 
     //set initial colors and declare positioning vars
     uint16_t color1 = 0x00FF;
@@ -25,6 +28,7 @@ int main(void) {
 
     lcd->clear();
 
+    //Draw random circles to screen forever.
     while(true) {
 
         ScanLine sline1;
@@ -32,17 +36,17 @@ int main(void) {
         sline1.horizontal = true;
         sline2.horizontal = true;
 
-        unsigned char circle_radius1 = rand() % max_circle_radius;
-        unsigned circle_radius2 = rand() % max_circle_radius;
+        unsigned int circle_radius1 = rand() % max_circle_radius;
+        unsigned int circle_radius2 = rand() % max_circle_radius;
 
-        segment_length = 0;
+        unsigned char segment_length = 0;
         for(i=0; i < circle_radius1; ++i) {
-            segment_length = sqrt(square(circle_radius1) - square(i));
+            segment_length = sqrt(stable[circle_radius1] - stable[i]);
             sline1.add_line(circle_radius1 - segment_length, 2 * segment_length);
         } 
 
         for(i=0; i < circle_radius2; ++i) {
-            segment_length = sqrt(square(circle_radius2) - square(i));
+            segment_length = sqrt(stable[circle_radius2] - stable[i]);
             sline2.add_line(circle_radius2 - segment_length, 2 * segment_length);
         }
 
